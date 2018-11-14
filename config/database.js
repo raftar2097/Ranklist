cec55d7149445c6ba92df2c6b6c0af81d3eaa65b
raftar2097 <ragarwala2097@gmail.com>
2018-11-15 01:14:04 +0530

    commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://rishab:224199520Rr@ds137220.mlab.com:37220/ranklist'}
+  module.exports = {mongoURI: 'mongodb://raftar2097:224199520Rr@ds137220.mlab.com:37220/ranklist1'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
-}
+} 
