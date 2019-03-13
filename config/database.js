45513faf44edf44c13ce78468ab6e160182899ce
raftar2097 <ragarwala2097@gmail.com>
2019-03-14 00:33:36 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://raftar2097:224199520Rr@ds029821.mlab.com:29821/ranklist1'}
+  module.exports = {mongoURI: ''}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 } 
